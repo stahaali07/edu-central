3e1732262e5617b2d7590df0aca6a9b6e91a1ea4
stahaali07 <staha.ali07@gmail.com>
2023-10-02 01:51:32 +0500

    User Activation

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,7 +1,9 @@
 import  express from "express";
-import { RegistrationUser } from "../controllers/user.controller";
+import { RegistrationUser, ActivateUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
 userRouter.post('/registration', RegistrationUser);
+
+userRouter.post('/activate-user', ActivateUser);
 
 export default userRouter;
