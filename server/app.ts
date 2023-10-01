9f1d65c37fbd47cc2beff5c01ca7b9701604f685
stahaali07 <staha.ali07@gmail.com>
2023-10-01 07:15:20 +0500

    Error Handlers

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
 
 // BODY PARSER
 app.use(express.json({ limit: "50mb" }));
@@ -31,3 +32,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+app.use(ErrorMiddleware);
