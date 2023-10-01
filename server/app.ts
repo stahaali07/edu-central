577299f5f5955437222b74d8942a0bc94a81e9ba
stahaali07 <staha.ali07@gmail.com>
2023-10-01 06:29:43 +0500

    MongoDB, Cloudinary, Redis connection

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,33 @@
-import express from "express";
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// BODY PARSER
+app.use(express.json({ limit: "50mb" }));
+
+// COOKIE PARSER
+app.use(cookieParser());
+
+// CORS
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// API TESTING
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+// UNKNOWN ROUTES
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
+import ConnectDB from "./utils/db";
 require("dotenv").config();
 
 // SERVER CREATION
 app.listen(process.env.PORT, () => {
-    console.log(`Server is connected with port ${process.env.PORT}`)
-})
+    console.log(`Server is connected with port ${process.env.PORT}`);
+    ConnectDB();
+});
