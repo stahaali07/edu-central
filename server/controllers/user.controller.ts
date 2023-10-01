98ebea512a20ff5ce435c56dd3e9d46a991e95af
stahaali07 <staha.ali07@gmail.com>
2023-10-02 03:21:19 +0500

    User Login and Logout

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -7,6 +7,7 @@ import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { SendToken } from "../utils/jwt";
 
 // REGISTER USER
 interface iRegistrationBody {
@@ -115,6 +116,55 @@ export const ActivateUser = CatchAsyncError(
 
       res.status(201).json({
         success: true,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// LOGIN USER
+interface ILogInRequest {
+  email: string;
+  password: string;
+}
+
+export const LoginUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body as ILogInRequest;
+      if (!email || !password) {
+        return next(new ErrorHandler("Please enter email and password", 400));
+      }
+
+      const user = await userModel.findOne({ email }).select("+password");
+
+      if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+
+      const isPasswordMatch = await user.comparePassword(password);
+
+      if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid email or password", 400));
+      }
+
+      SendToken(user, 200, res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// LOGOUT USER
+export const LogoutUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.cookie("access_token", "", { maxAge: 1 });
+      res.cookie("refresh_token", "", { maxAge: 1 });
+      res.status(200).json({
+        success: true,
+        message: "Logged out successfully",
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,13 @@
 import  express from "express";
-import { RegistrationUser, ActivateUser } from "../controllers/user.controller";
+import { RegistrationUser, ActivateUser, LoginUser, LogoutUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
 userRouter.post('/registration', RegistrationUser);
 
 userRouter.post('/activate-user', ActivateUser);
+
+userRouter.post('/login', LoginUser);
+
+userRouter.get('/logout', LogoutUser);
 
 export default userRouter;
